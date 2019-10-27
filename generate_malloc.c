3db394ed70b8893172272906d869a9cef369c495
jzamora5 <jzamora_5@yahoo.com>
2019-10-27 02:43:08 +0000

    [Feature] Generate Malloc of Specifier

---
+++ generate_malloc.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * generate_malloc - calls function that creates malloc with written specifier
+ *
+ * @s: parameter to analize %+3.3d
+ * @len_p: length of part analized %+5.4d (l = 6)
+ * @list: list of variadic arguments
+ *
+ * Return: 1 if correct, 0 if incorrect
+ */
+char *generate_malloc(char *s, int len_p, va_list list)
+{
+	int i;
+	char *(*f)(char *, int, va_list);
+	char *fx = 0;
+	specs_t tspecs[] = {
+		{'c', gen_c},
+		{'s', gen_s},
+		{'S', gen_S},
+		{'d', gen_i},
+		{'i', gen_i},
+		{'o', gen_o},
+		{'x', gen_x},
+		{'X', gen_X},
+		{'b', gen_b},
+		{'u', gen_u},
+		{'p', gen_p},
+		{'r', gen_r},
+		{'R', gen_R},
+	};
+	/* 13 Specifiers */
+	for (i = 0; i < 13; i++)
+		if (s[len_p - 1] == tspecs[i].s)
+		{
+			f = tspecs[i].f;
+			fx = f(s, len_p, list);
+		}
+	/* Returns Pointer Malloec with number or string */
+	return (fx);
+}
