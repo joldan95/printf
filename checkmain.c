9933d6a8fb11da47ae0aaa74f6f0c74a690de368
joldan95 <malf95@hotmail.com>
2019-10-28 02:52:46 -0500

    [Update]Testing file

--- checkmain.c
+++ checkmain.c
@@ -1,8 +1,42 @@
 #include "holberton.h"
-
+#include <stdio.h>
 int main(void)
 {
-	int n = 2;
-	_printf("Hello c: %c s: %s %: %% d: %d o: %o b: %b x: %x X: %X p: %p u: %u S: %S World\n", 35, 0, -54, 54, 54, 11238, 11238, &n, -1, "Holberton\nSchool");
+	/* int n = 2; */
+	/* _printf("Character: %helloc\n", 35); */
+	/* _printf("String: %s\n", "Hola Mundo"); */
+	/* _printf("Percentage: %%\n"); */
+	/* _printf("Integer: %d\n", 50); */
+	/* _printf("Octal: %o\n", -50); */
+	/* _printf("Binary: %b\n", 50); */
+	/* _printf("Hexadecimal lower: %x\n", 50); */
+	/* _printf("Hexadecimal upper: %X\n", 50); */
+	/* _printf("Mem direction: %p\n", (*main)); */
+	/* _printf("Unsigned: %u\n", 50); */
+	/* _printf("String non-printables: %S\n", "Hello\nWorld\t"); */
+	/* _printf("-------------------\n"); */
+	/* _printf("c: %Helloc s: %s %: %% d: %d o: %o b: %b x: %x X: %X p: %p u: %u\n",/\* 35, *\/"Hello World", -54, 54, 54, 11238, -5, &n, -1); */
+
+	_printf("----------------\nTest for integers\n----------------\n");
+	_printf("Normal: %.5s\n", "Hola Mundo");
+	_printf("Normal: %.2d\n", 578);
+	_printf("Normal: %.9d\n", -578);
+	_printf("Normal: %.2d\n", -578);
+	/* _printf("Long: %ld\n", 123456789123); */
+	/* _printf("Short: %hd\n", 123456789123); */
+	/* _printf("----------------\nTest for Octals\n----------------\n"); */
+	/* _printf("Normal: %o\n", 123456789123); */
+	/* _printf("Long: %lo\n", 123456789123); */
+	/* _printf("Short: %ho\n", 123456789123); */
+	/* _printf("----------------\nTest for hexa\n----------------\n"); */
+	/* _printf("Normal: %x\n", 123456789123); */
+	/* _printf("Long: %lx\n", 123456789123); */
+	/* _printf("Short: %hx\n", 123456789123); */
+	/* _printf("----------------\nTest for hexa\n----------------\n"); */
+	/* _printf("Normal: %X\n", 123456789123); */
+	/* _printf("Long: %lX\n", 123456789123); */
+	/* _printf("Short: %hX\n", 123456789123); */
+
+
 	return (0);
 }
