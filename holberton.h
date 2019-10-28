a8e5fe1c7a6d57a03da63a30592661e6994bfb1e
jzamora5 <jzamora_5@yahoo.com>
2019-10-28 21:29:13 +0000

    Header File c case null

--- holberton.h
+++ holberton.h
@@ -26,7 +26,7 @@ typedef struct Checkstr
 typedef struct Specs_t
 {
 	char s;
-	char *(*f)(const char *, int, va_list);
+	char *(*f)(const char *, int, va_list, int *);
 } specs_t;
 /* Custom printf function */
 int _printf(const char *format, ...);
@@ -45,20 +45,20 @@ void print_number_str(long int n, char *str);
 void print_number_str_u(unsigned long int n, char *str);
 
 /* Generate strings functions */
-char *generate_malloc(const char *s, int len_p, va_list list);
-char *gen_c(const char *, int, va_list);
-char *gen_s(const char *, int, va_list);
-char *gen_d(const char *, int, va_list);
-char *gen_i(const char *, int, va_list);
-char *gen_o(const char *, int, va_list);
-char *gen_b(const char *, int, va_list);
-char *gen_x(const char *, int, va_list);
-char *gen_X(const char *, int, va_list);
-char *gen_p(const char *, int, va_list);
-char *gen_u(const char *, int, va_list);
-char *gen_S(const char *, int, va_list);
-char *gen_r(const char *, int, va_list);
-char *gen_R(const char *, int, va_list);
+char *generate_malloc(const char *s, int len_p, va_list list, int *);
+char *gen_c(const char *, int, va_list, int *);
+char *gen_s(const char *, int, va_list, int *);
+char *gen_d(const char *, int, va_list, int *);
+char *gen_i(const char *, int, va_list, int *);
+char *gen_o(const char *, int, va_list, int *);
+char *gen_b(const char *, int, va_list, int *);
+char *gen_x(const char *, int, va_list, int *);
+char *gen_X(const char *, int, va_list, int *);
+char *gen_p(const char *, int, va_list, int *);
+char *gen_u(const char *, int, va_list, int *);
+char *gen_S(const char *, int, va_list, int *);
+char *gen_r(const char *, int, va_list, int *);
+char *gen_R(const char *, int, va_list, int *);
 
 /* Flags functions */
 unsigned int get_width(const char *pattern, int len_p, va_list list);
